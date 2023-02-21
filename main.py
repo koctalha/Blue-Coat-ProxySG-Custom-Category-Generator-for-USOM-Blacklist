2d2346cd6785bb8929176ab81a8cd521b64aafa4
koctalha <talhakoc@msn.com>
2023-02-21 18:48:47 +0300

    Corrupted URL formats parsed correctly.

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
 import wget
 import os
+import shutil
 
-url = 'https://www.usom.gov.tr/url-list.txt'
-filename = wget.download(url)
-with open("url-list.txt", "r") as file:
-    lines = file.read().split("\n")
-    newlines = ["define category USOM_URL_BLOCK_LIST"]
-    for line in lines:
-        line = line.rstrip()
-        newline = '\t' + line
-        newlines.append(newline)
-    newlines = newlines[:-1]
-    newlines.append("end")
-with open("clean-url-list.txt", "w") as newfile:
-    newfile.write("\n".join(newlines))
-os.remove('url-list.txt')
+url1 = 'https://www.usom.gov.tr/url-list.txt'
+
+dosya1_adi = "url-list.txt"
+baslangic_verisi1 = "define category USOM_URL_BLOCK_LIST\n"
+bitis_verisi = "\nend\n"
+
+# Dosya1'i okuyun
+try:
+    filename = wget.download(url1)
+    with open(dosya1_adi, "r") as dosya1:
+        icerik1 = dosya1.readlines()
+        with open("proxysgdbtmp.txt", "a") as writeFile:
+            writeFile.write(baslangic_verisi1)
+            for icerik in icerik1:
+                icerik = icerik.split('/')
+                cleanicerik = icerik[0]
+                #print(cleanicerik)
+                if ":" in cleanicerik:
+                    continue
+                else:
+                    if len(cleanicerik)>128:
+                        print(cleanicerik)
+                        cleanicerik = cleanicerik.split('.')
+                        i = len(cleanicerik)
+                        cleanicerik = str(cleanicerik[i-3])+"."+str(cleanicerik[i-2])+"."+str(cleanicerik[i-1])
+                        print(cleanicerik)
+                    cleanicerik = cleanicerik+"\r"
+                    writeFile.write(cleanicerik)
+
+            writeFile.write(bitis_verisi)
+except:
+    pass
+
+shutil.copyfile("proxysgdbtmp.txt","proxysgdb.txt")
+
+os.remove("url-list.txt")
+os.remove("proxysgdbtmp.txt")
